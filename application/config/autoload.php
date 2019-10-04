e7d07f3ec8a3d95a2924270f36a4596140d22985
yogeswaran <yogadev05@gmail.com>
2019-10-04 17:15:46 +0530

    autoload updated

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session','mailer');
+$autoload['libraries'] = array('database', 'session','datatables','mailer');
 
 /*
 | -------------------------------------------------------------------
