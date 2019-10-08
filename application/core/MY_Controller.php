17d65cb13bef841ef23f365d39b3ad562a15f5f4
Yogeswaran <yogadev05@gmail.com>
2019-10-08 05:59:05 +0000

    Apply fixes from StyleCI

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,13 +1,12 @@
 <?php
+
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class MY_Controller extends MX_Controller
 {
-
     public function __construct()
     {
         parent::__construct();
-        log_message('debug', "MY_Controller Initialized");
+        log_message('debug', 'MY_Controller Initialized');
     }
-
 }
--- commands/Module.php
+++ commands/Module.php
@@ -3,12 +3,10 @@
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Module extends Command
 {
-
     private $name;
     private $error;
 
@@ -26,7 +24,7 @@ class Module extends Command
 
         $this->_check_class();
         if (!empty($this->error) && $this->error != '') {
-            $output->writeln("<error>" . $this->error . "</error>");
+            $output->writeln('<error>'.$this->error.'</error>');
         } else {
             $this->createModule();
             $output->writeln("<info>'$this->name' Module created successfully!</info>");
@@ -39,7 +37,7 @@ class Module extends Command
         mkdir($prev_path.$this->name.'/');
         mkdir($prev_path.$this->name.'/controllers\/');
         mkdir($prev_path.$this->name.'/views\/');
-$html='
+        $html = '
 <html>
     <head>
         <title>403 Forbidden</title>
@@ -50,27 +48,27 @@ $html='
 
     </body>
 </html>';
-        file_put_contents( $prev_path.$this->name.'/views\/index.html', trim($html));
+        file_put_contents($prev_path.$this->name.'/views\/index.html', trim($html));
     }
 
     private function _check_class()
     {
-        
-        if(!is_dir('application/modules/')){
+        if (!is_dir('application/modules/')) {
             $this->error = 'Modules Folder doesn\'t exists!';
+
             return;
         }
 
-        if(strstr($this->name, '/')){
+        if (strstr($this->name, '/')) {
             $this->error = 'Sub-Folder in module cannot be created!';
+
             return;
         }
 
-        if (is_dir('application/modules/' . $this->name)) {
+        if (is_dir('application/modules/'.$this->name)) {
             $this->error = 'Module with this name already exists!';
+
             return;
         }
-
-        return null;
     }
 }
